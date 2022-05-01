574673189026e63918c9bd41e8782a41805b4c53
Alexey Dolgiy <alexey.dolgiy@gmail.com>
2022-05-01 09:13:42 +0300

    update store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 // import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 import App from './components/App/App';
 // import reportWebVitals from './reportWebVitals';
 import './index.css';
